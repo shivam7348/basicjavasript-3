e97f6e2e6b4a24b039ff6eea578a50e1179d5557
Shivam Kumar <106791774+shivam7348@users.noreply.github.com>
2024-01-06 21:45:39 +0000

    basic fnc

---
+++ arrowfn.js
@@ -0,0 +1,45 @@
+// function chai() {
+//     username = "Shivam"
+//     console.log(this.username);
+
+// }
+// chai()
+// const chai = function () {
+   // let username = "shivam"
+    //console.log(this.username)
+    //undefined after console
+
+
+//}
+//chai()
+//to make arrow function
+
+const chai =  ( ) => {
+    let username = "satyam"
+    console.log(this.username);
+}
+//  chai()
+//pure Arrow Function
+
+ const addTwo =  ( num1, num2) => {
+    return num1 + num2
+    
+ }
+ // to print we have to print the valuw with the console
+   console.log(addTwo(5, 7))
+
+
+   
+
+
+
+
+
+
+
+
+
+
+ 
+
+
